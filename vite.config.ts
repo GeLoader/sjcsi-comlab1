31d926db2e2a823c73809623eae2c6015cc05cef
GeLoader <geloader1@gmail.com>
2025-07-01 17:01:18 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,8 @@ export default defineConfig(({ mode }) => ({
   //   host: "::",
   //   port: 8080,
   // },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  base: "./",
+  plugins: [react()],
+  base: "/sjcsi-comlab1",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
