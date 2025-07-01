bec2affdac00bc4cb05ec064bb19f7d4abf17e5a
GeLoader <geloader1@gmail.com>
2025-07-01 16:54:57 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/sjcsi-comlab1",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
